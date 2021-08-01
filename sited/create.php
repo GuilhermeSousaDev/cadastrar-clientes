64cfb044dfce1fada914be9702696a6ed80d5067
Guilherme Augusto Rocha Sousa <a.guilhermerocha250@gmail.com>
2021-08-01 15:51:19 -0300

    Add files via upload

--- sited/create.php
+++ sited/create.php
@@ -1,11 +1,17 @@
 <?php
 session_start();
 include_once 'db_connect.php';
+function clear($input) {
+    global $conn;
+    $var = mysqli_escape_string($conn,$input);
+    $var = htmlspecialchars($var);
+    return $var;
+}
 if(isset($_POST['btn-cadastrar'])) {
-    $nome = mysqli_escape_string($conn, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($conn, $_POST['sobrenome']);
-    $email = mysqli_escape_string($conn, $_POST['email']);
-    $idade = mysqli_escape_string($conn, $_POST['idade']);
+    $nome = clear($_POST['name']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
     
     $sql = "INSERT INTO clientes(nome,sobrenome,email,idade) VALUES('$nome','$sobrenome','$email',$idade)";
     mysqli_query($conn,$sql);
